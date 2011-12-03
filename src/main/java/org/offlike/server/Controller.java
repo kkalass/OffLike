6f8e93d6964d8936c4af2c77056815d7e45a3701
Klas Kalass <klas.kalass@freiheit.com>
2011-12-03 17:22:25 +0100

    integrated jsp views, added example, added utility libs

--- src/main/java/org/offlike/server/Controller.java
+++ src/main/java/org/offlike/server/Controller.java
@@ -1,15 +1,17 @@
 package org.offlike.server;
 
+import org.joda.time.DateTime;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.google.common.collect.ImmutableMap;
 
 @org.springframework.stereotype.Controller
 public class Controller {
 
-	@RequestMapping("/hello")
-	@ResponseBody
-	public String hello(){
-		return "hello";
+	@RequestMapping("/")
+	public ModelAndView hello(){
+		return new ModelAndView("index",  ImmutableMap.<String, Object>of("name", "Test User", "Datum", new DateTime()));
 	}
 	
 }
