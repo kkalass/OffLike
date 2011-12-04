417fda20c7a4fe55c14e7ba2bed9181d09a88048
me4bruno <a.bruns@gmx.de>
2011-12-04 03:01:08 +0100

    MongoDB generateQrCode korrigiert

--- src/main/java/org/offlike/server/service/QrCodeService.java
+++ src/main/java/org/offlike/server/service/QrCodeService.java
@@ -1,26 +1,36 @@
 package org.offlike.server.service;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
-
-import javax.imageio.ImageIO;
+import java.net.URLEncoder;
 
 import org.offlike.server.data.Campaign;
+import org.offlike.server.data.QrCode;
 
 public class QrCodeService {
 
-	private static final String GOOGLE_QR_API_URL = "http://chart.apis.google.com/chart?cht=qr&chs=350x350&chld=L&choe=UTF-8&chl=http%3A%2F%2Fofflike.org%2Fcampaign%2F";
-
-	public void generateQrCode(Campaign campaign, String fileName) throws Exception {
-		String generatedCampagneId = "" + campaign.getId();
-		URL url = new URL(GOOGLE_QR_API_URL + generatedCampagneId);
-		BufferedImage read = ImageIO.read(url);
-		ImageIO.write(read, "png", new File(fileName));	
+	private static final String URL_ENCODING = "UTF-8";
+	private static final String GOOGLE_QR_API = "http://chart.apis.google.com/chart?cht=qr&chs=350x350&chld=L&choe=UTF-8&chl=";
+	private static final String OFFLIKE_DOMAIN = "http://offlike.org/like/";
+	
+	private final MongoDbService mongoDbService;
+	
+	public QrCodeService(MongoDbService mongoDbService) {
+		this.mongoDbService = mongoDbService;
 	}
 	
-	public static void main(String[] args) throws Exception {
-		Campaign campaign = new Campaign();
-		new QrCodeService().generateQrCode(campaign, "qrCode.png");
+	public URL generateQrCode(String campaignId) throws Exception {
+		QrCode qrCode = new QrCode();
+		Campaign campaign = mongoDbService.findCampaignById(campaignId);
+		mongoDbService.createQrCode(campaign, qrCode);
+		
+		return new URL(GOOGLE_QR_API + parseUrl(qrCode.getId(), campaign.getTitle()));
+	}
+	
+	protected static String parseUrl(String qrCodeId, String campaignTitle) throws UnsupportedEncodingException {
+		String URLdomain = URLEncoder.encode(OFFLIKE_DOMAIN+qrCodeId,URL_ENCODING);
+		String URLcamapignTitle = URLEncoder.encode(campaignTitle, URL_ENCODING);
+		
+		return URLdomain + "?campaign_name=" + URLcamapignTitle;
 	}
 }
